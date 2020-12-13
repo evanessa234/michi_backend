332ea2e51c2af932a979d75d2d4461aa60fba51a
Mukulika <mukulikapahari@gmail.com>
2020-12-13 14:12:57 +0530

    fixed: authentication callback

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "https://michi-backend.herokuapp.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     /* register user
