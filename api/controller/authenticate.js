a2852615c92acad79eccf208a305e00ce2753a25
Mukulika <mukulikapahari@gmail.com>
2020-12-13 14:10:20 +0530

    added: config vars

--- api/controller/authenticate.js
+++ api/controller/authenticate.js
@@ -21,8 +21,8 @@ passport.deserializeUser((user, done) => {
 })
 
 passport.use(new GoogleStrategy({
-    clientID: config.auth.GOOGLE_CLIENT_ID,
-    clientSecret: config.auth.GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
